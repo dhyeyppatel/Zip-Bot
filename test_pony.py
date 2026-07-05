247dcaa59bfffc40874eeb53c61fb8f9e0f3757c
dhyeyppatel <dhyeyp254@gmail.com>
2026-07-06 02:46:14 +0530

    Fix: Remove stale session file and test script; use in_memory session on Render

--- test_pony.py
+++
@@ -1,5 +0,0 @@
-import asyncio; from pony.orm import *; db = Database(); db.bind('sqlite', ':memory:', create_db=True); class User(db.Entity): pass; db.generate_mapping(create_tables=True); 
-async def test():
-  with db_session:
-    pass
-asyncio.run(test())
